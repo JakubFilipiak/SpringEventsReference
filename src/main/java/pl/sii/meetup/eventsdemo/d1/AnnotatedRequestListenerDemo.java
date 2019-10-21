36578267029fa12e0ed24f37320fa9ba00ee5693
Sergiusz Roszczyk <sroszczyk@sii.pl>
2019-10-21 18:28:26 +0200

    Demo 2: Annotated Event Handler

---
+++ src/main/java/pl/sii/meetup/eventsdemo/d1/AnnotatedRequestListenerDemo.java
@@ -0,0 +1,16 @@
+package pl.sii.meetup.eventsdemo.d1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
+
+@Slf4j
+@Service
+public class AnnotatedRequestListenerDemo {
+
+    @EventListener
+    public void onApplicationEvent( ServletRequestHandledEvent event ) {
+        log.info( "Servlet Request Handled Event received: {}", event );
+    }
+}
